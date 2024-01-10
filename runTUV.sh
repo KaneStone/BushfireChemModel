ff771d928b6b116c163f56d55613fc97a5366d4a
Kane Stone <kaneastone88@gmail.com>
2024-01-10 18:38:55 -0500

    update to runTUV path

--- runTUV.sh
+++ runTUV.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-cd /Users/kanestone/Dropbox\ \(MIT\)/Work_Share/MITWork/BushChemModel/TUV5.4/
+# cd /Users/kanestone/Dropbox\ \(MIT\)/Work_Share/MITWork/BushChemModel/TUV8.4/
+cd TUV5.4/
 
 ./tuv
 
